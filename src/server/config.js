2f5a40a77e736db74d9f21d93ad059942ad81eb1
TheoKondak <thkondak@gmail.com>
2020-09-13 17:03:43 +0200

    Project created with create-js-app

---
+++ src/server/config.js
@@ -0,0 +1,15 @@
+/* @flow */
+
+function notEmpty(value: ?string): string {
+  if (value == null) {
+    throw new Error('Has to be nonempty string');
+  }
+
+  return value;
+}
+
+export const ASSETS_PATH: string = notEmpty(process.env.ASSETS_PATH);
+export const ASSETS_DIR: string = notEmpty(process.env.ASSETS_DIR);
+export const IS_DEVELOPMENT: boolean = notEmpty(process.env.NODE_ENV) === 'development';
+export const PUBLIC_DIR: string = notEmpty(process.env.PUBLIC_DIR);
+export const SERVER_PORT: number = parseInt(notEmpty(process.env.SERVER_PORT), 10);
